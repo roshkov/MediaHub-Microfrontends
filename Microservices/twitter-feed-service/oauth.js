e0661edc1d91036add48ddfbe5bd86cb9cc94ad7
roshkov <nikitar98@inbox.lv>
2023-10-30 19:34:12 +0100

    can authenticate and get access token, refresh token

--- Microservices/twitter-feed-service/oauth.js
+++ Microservices/twitter-feed-service/oauth.js
@@ -1,98 +1,78 @@
-// import { twitterConfig } from "./utils/constants";
+// // import { twitterConfig } from "./utils/constants";
 
 // const express = require("express");
 // const session = require("express-session");
 // const passport = require("passport");
 // const TwitterStrategy = require("passport-twitter").Strategy;
+// const dotenv = require("dotenv");
+
+// dotenv.config();
 
 // const app = express();
+// const port = process.env.PORT || 3100;
 
 // app.use(
-//   session({ secret: "your-secret-key", resave: true, saveUninitialized: true })
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: true,
+//     saveUninitialized: true,
+//   })
 // );
 // app.use(passport.initialize());
 // app.use(passport.session());
 
+// const twitterConfig = {
+//   consumerKey: process.env.TWITTER_CONSUMER_KEY,
+//   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+//   callbackURL: process.env.CALLBACK_URL,
+// };
+// console.log("twitterConfig.consumerKey  ", twitterConfig.consumerKey);
+
 // passport.use(
 //   new TwitterStrategy(twitterConfig, (token, tokenSecret, profile, done) => {
-//     // Your logic to handle user data and authentication
-//     // Typically, you'd create or update a user in your database here
+//     // Handle user data and authentication here.
+//     // This is where you would create or update a user in your database.
+//     console.log("token", token);
+//     console.log("profile", profile);
 //     return done(null, profile);
 //   })
 // );
 
-// import { twitterConfig } from "./utils/constants";
-const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const TwitterStrategy = require("passport-twitter").Strategy;
-const dotenv = require("dotenv");
+// // Define the Twitter authentication route
+// // app.get("/auth/twitter", passport.authenticate("twitter"));
 
-dotenv.config();
+// app.get("/auth/twitter", (req, res, next) => {
+//   passport.authenticate("twitter", (err, user, info) => {
+//     if (err) {
+//       console.error(err);
+//       console.log("what happened: ", err, " user", user, "  info ", info);
+//       // Handle the error here
+//       // You can, for example, redirect to an error page
+//       return res.redirect("/error");
+//     }
+//     if (!user) {
+//       console.error(info);
+//       // Handle the info here
+//       // You can, for example, redirect to an info page or show a message
+//       return res.redirect("/info");
+//     }
+//     // Successful authentication, this part will not execute unless successful
+//     // In case of success, it will continue to the callback route
+//   })(req, res, next);
+// });
 
-const app = express();
-const port = process.env.PORT || 3100;
+// // Handle the callback route after Twitter authentication
+// app.get(
+//   "/auth/twitter/callback",
+//   passport.authenticate("twitter", { failureRedirect: "/" }),
+//   (req, res) => {
+//     // Successful authentication, redirect or respond as needed
+//     res.redirect("/profile");
+//   }
+// );
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.listen(port, () => {
+//   console.log(`OAuth server is running on port ${port}`);
+// });
 
-const twitterConfig = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  callbackURL: process.env.CALLBACK_URL,
-};
-console.log("twitterConfig.consumerKey  ", twitterConfig.consumerKey);
-
-passport.use(
-  new TwitterStrategy(twitterConfig, (token, tokenSecret, profile, done) => {
-    // Handle user data and authentication here.
-    // This is where you would create or update a user in your database.
-    console.log("token", token);
-    console.log("profile", profile);
-    return done(null, profile);
-  })
-);
-
-// Define the Twitter authentication route
-// app.get("/auth/twitter", passport.authenticate("twitter"));
-
-app.get("/auth/twitter", (req, res, next) => {
-  passport.authenticate("twitter", (err, user, info) => {
-    if (err) {
-      // console.error(err);
-      console.log("what happened: ", err, " user", user, "  info ", info);
-      // Handle the error here
-      // You can, for example, redirect to an error page
-      // return res.redirect("/error");
-    }
-    if (!user) {
-      console.error(info);
-      // Handle the info here
-      // You can, for example, redirect to an info page or show a message
-      // return res.redirect("/info");
-    }
-    // Successful authentication, this part will not execute unless successful
-    // In case of success, it will continue to the callback route
-  })(req, res, next);
-});
-
-// Handle the callback route after Twitter authentication
-app.get(
-  "/auth/twitter/callback",
-  passport.authenticate("twitter", { failureRedirect: "/" }),
-  (req, res) => {
-    // Successful authentication, redirect or respond as needed
-    res.redirect("/profile");
-  }
-);
-
-app.listen(port, () => {
-  console.log(`OAuth server is running on port ${port}`);
-});
+// // OLD (up)
---
+++ Microservices/twitter-feed-service/server.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const passport = require("passport");
+const { Strategy } = require("@superfaceai/passport-twitter-oauth2");
+const session = require("express-session");
+require("dotenv").config();
+
+// <1> Serialization and deserialization
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+// Use the Twitter OAuth2 strategy within Passport
+passport.use(
+  // <2> Strategy initialization
+  new Strategy(
+    {
+      clientID: process.env.TWITTER_CONSUMER_KEY,
+      clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+      clientType: "confidential",
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    // <3> Verify callback
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Success!", { accessToken, refreshToken });
+      //because of this, later we can do req.user.accessToken
+      const user = {
+        accessToken,
+        refreshToken,
+        profile,
+      };
+      return done(null, user);
+    }
+  )
+);
+
+const app = express();
+const port = process.env.PORT || 3100;
+
+// <4> Passport and session middleware initialization
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// <5> Start authentication flow
+app.get(
+  "/auth/twitter",
+  passport.authenticate("twitter", {
+    // <6> Scopes
+    scope: ["tweet.read", "users.read", "offline.access"],
+  })
+);
+
+// <7> Callback handler
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter"),
+  function (req, res) {
+    const userData = JSON.stringify(req.user, undefined, 2);
+
+    const accessToken = req.user.accessToken;
+    const refreshToken = req.user.refreshToken;
+
+    // console.log("access token console ", req.user.accessToken);
+
+    // res.redirect(
+    //   `http://localhost:1001/?userData=${encodeURIComponent(userData)}`
+    // );
+
+    res.redirect(
+      `http://localhost:1001/oauth-callback?accessToken=${accessToken}&refreshToken=${refreshToken}`
+    );
+
+    // res.end(
+    //   `<h1>Authentication succeeded</h1> User access token: <pre>${req.user.accessToken}</pre>`
+    // );
+  }
+);
+
+app.listen(port, () => {
+  console.log(`server.js is running on port ${port}`);
+});
